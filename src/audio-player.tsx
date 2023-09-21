7406b3c134400884526b1c38110e1c75892434ed
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-09-21 13:12:52 +0300

    integrate seekbar and buttons

--- src/audio-player.tsx
+++ src/audio-player.tsx
@@ -1,7 +1,6 @@
 import {BasePlugin, KalturaPlayer} from '@playkit-js/kaltura-player-js';
 import {AudioPlayerConfig} from './types';
 import {AudioPlayerView} from './components';
-import {AudioSeekbar} from './components/audio-seekbar';
 
 class AudioPlayer extends BasePlugin<AudioPlayerConfig> {
   constructor(name: string, player: KalturaPlayer, config: AudioPlayerConfig) {
@@ -31,7 +30,9 @@ class AudioPlayer extends BasePlugin<AudioPlayerConfig> {
     // @ts-ignore
     this.player.ui._uiManager.buildCustomUI([
       {
-        template: () => <AudioPlayerView pluginConfig={this.config}/>,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        template: () => <AudioPlayerView props={{}} />,
         condition: () => true
       }
     ]);
--- src/components/audio-player-view/audio-player-view.tsx
+++ src/components/audio-player-view/audio-player-view.tsx
@@ -1,25 +1,56 @@
-// import {useState, useEffect, useContext} from 'preact/hooks';
-// import * as styles from './audio-player-preset.scss';
+import * as styles from './audio-player-view.scss';
+
+import {ui} from '@playkit-js/kaltura-player-js';
+const {connect} = ui.redux;
+
 import {AudioSeekbar} from '../audio-seekbar';
 import {AudioPlayerControls} from '../audio-player-controls';
 
-//import {BasePlugin, KalturaPlayer, ui} from '@playkit-js/kaltura-player-js';
+const {PLAYER_SIZE} = ui.Components;
 
-/**
- * Image with fixed dimensions and a fallback option for images which failed to load.
- *
- * @param {object} props Component props.
- * @param {string} props.poster Base image url.
- */
+const mapStateToProps = (state: any) => {
+  const {shell} = state;
 
-const AudioPlayerView = ({pluginConfig}: any) => {
+  let sizeClass = '';
+  switch (shell.playerSize) {
+    case PLAYER_SIZE.EXTRA_LARGE:
+    case PLAYER_SIZE.LARGE:
+    case PLAYER_SIZE.MEDIUM: {
+      sizeClass = styles.medium;
+      break;
+    }
+    case PLAYER_SIZE.SMALL: {
+      sizeClass = styles.small;
+      break;
+    }
+    case PLAYER_SIZE.EXTRA_SMALL: {
+      sizeClass = styles.extraSmall;
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+
+  return {
+    sizeClass
+  };
+};
+
+interface AudioPlayerViewProps {
+  sizeClass?: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const AudioPlayerView = connect(mapStateToProps)(({sizeClass}: AudioPlayerViewProps) => {
   return (
-    <div className="my-preset" style="position: relative; background-color: red; height: 160px; margin: 0">
+    <div className={`${styles.audioPlayerView} ${sizeClass}`} style="position: relative; background-color: red">
       <AudioSeekbar />
-      <AudioPlayerControls pluginConfig={pluginConfig} />
+      <AudioPlayerControls pluginConfig={{}} />
     </div>
   );
-};
+});
 
 AudioPlayerView.displayName = 'AudioPlayer';
 
