d1b7c948406bba71d7c39bbccf41f6543f8f8ee9
JonathanTGold <jonathan.gold@86@gmail.com>
2024-04-09 20:03:05 +0300

    add Accessibility

--- src/components/plugins/menu-item/menu-item.tsx
+++ src/components/plugins/menu-item/menu-item.tsx
@@ -1,5 +1,6 @@
 import {ui} from '@playkit-js/kaltura-player-js';
 import * as styles from './menu-item.scss';
+import {A11yWrapper} from '@playkit-js/common/dist/hoc/a11y-wrapper';
 
 //@ts-ignore
 const {Icon, IconType} = ui.Components;
@@ -12,16 +13,18 @@ interface MenuItemProps {
 
 const MenuItem = ({pluginName, svgUrl, onClick}: MenuItemProps) => {
   return (
-    <div onClick={onClick} className={styles.menuItem}>
-      <span className={styles.pluginIcon}>
-        <Icon id={`min-audio-player-download`} path={svgUrl} viewBox={`0 0 32 32`} />
-      </span>
-      <div className={styles.content2}>{pluginName}</div>
-      <span className={styles.arrowIcon}>
-        {/*@ts-ignore*/}
-        <Icon type={IconType.ArrowDown} />
-      </span>
-    </div>
+    <A11yWrapper onClick={onClick}>
+      <div className={styles.menuItem} tabIndex={0} aria-label={pluginName}>
+        <span className={styles.pluginIcon}>
+          <Icon id={`min-audio-player-download`} path={svgUrl} viewBox={`0 0 32 32`} />
+        </span>
+        <div className={styles.content2}>{pluginName}</div>
+        <span className={styles.arrowIcon}>
+          {/*@ts-ignore*/}
+          <Icon type={IconType.ArrowDown} />
+        </span>
+      </div>
+    </A11yWrapper>
   );
 };
 
