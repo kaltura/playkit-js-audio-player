1359e6535f1486bf19535751bb23905f5c8514a1
JonathanTGold <jonathan.gold@86@gmail.com>
2024-05-23 11:33:53 +0300

    fix(FEC-13955): Update Demo to support light theme

--- src/audio-player.tsx
+++ src/audio-player.tsx
@@ -38,21 +38,6 @@ class AudioPlayer extends BasePlugin {
     });
   }
 
-  /**
-   * Return a hex color as an array of Red, Green and Blue values
-   * @param {string} color  - 3 or 6 digit hex color value
-   * @returns {number[]} array of RGB numeric values
-   */
-  private getColorAsRGB(color: string) {
-    let fullHexColor = color;
-    if (color.length === 4) {
-      fullHexColor = `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${color[3]}`;
-    }
-
-    const colorArr = fullHexColor.match(/^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i);
-    return colorArr ? [parseInt(colorArr[1], 16), parseInt(colorArr[2], 16), parseInt(colorArr[3], 16)] : null;
-  }
-
   private prepareUI() {
     if (!this.colorVariablesSet) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -66,22 +51,15 @@ class AudioPlayer extends BasePlugin {
       // @ts-ignore
       this.eventManager.listenOnce(this.player, core.EventType.CHANGE_SOURCE_ENDED, () => {
         const colorHexAsFilter = hexToCSSFilter(color, {acceptanceLossPercentage: 1}).filter;
-        const colorHexAsRGB = this.getColorAsRGB(color);
+        // const colorHexAsRGB = this.getColorAsRGB(color);
 
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         this.player.ui._uiManager.setCSSVariable(CONTROLS_FILTER_COLOR_VARIABLE, colorHexAsFilter.replace(';', ''));
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        this.player.ui._uiManager.setCSSVariable(TONE_1_COLOR_RGB_VARIABLE, colorHexAsRGB);
-
         if (
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-          this.player.ui._uiManager.getCSSVariable(CONTROLS_FILTER_COLOR_VARIABLE) &&
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          this.player.ui._uiManager.getCSSVariable(TONE_1_COLOR_RGB_VARIABLE)
+          this.player.ui._uiManager.getCSSVariable(CONTROLS_FILTER_COLOR_VARIABLE)
         ) {
           this.colorVariablesSet = true;
         }
