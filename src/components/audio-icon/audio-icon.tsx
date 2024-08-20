522252544df8d4c893354c5167557b03b5c51037
SudoSaysWhat <razvan.brinzoi@kaltura.com>
2024-08-20 16:17:40 +0300

    ADA-1380

--- src/components/audio-icon/audio-icon.tsx
+++ src/components/audio-icon/audio-icon.tsx
@@ -1,5 +1,6 @@
 import {h} from 'preact';
 import * as styles from './audio-icon.scss';
+import { useState } from 'preact/hooks';
 
 interface AudioIconProps {
   isLarge: boolean;
@@ -7,12 +8,20 @@ interface AudioIconProps {
 }
 
 const AudioIcon = ({isLarge, isActive}: AudioIconProps) => {
+  const [isAnimation, setIsAnimation] = useState(true);
+
+  const handleClick = () => {
+    if(isActive){
+      setIsAnimation(prevState => !prevState);
+    };
+  };
+  
   return (
-    <div data-testid="audio-player-audio-icon" className={`${styles.audioIcon} ${isLarge ? styles.large : ''} ${isActive ? styles.active : ''}`}>
+    <button onClick={handleClick} aria-label={(isAnimation && isActive) ? "Pause Animation" : "Play Animation"} data-testid="audio-player-audio-icon" className={`${styles.audioIcon} ${isLarge ? styles.large : ''} ${(isAnimation && isActive) ? styles.active : ''}`}>
       <div className={styles.box2} />
       <div className={styles.box1} />
       <div className={styles.box2} />
-    </div>
+    </button>
   );
 };
 
